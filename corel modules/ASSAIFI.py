85097d77c966225270033bc70b01e62bc8660200
Keykeykeykeykeykey <lhxqy.ok@gmail.com>
2025-11-20 17:20:16 +0800

    Create ASSAIFI.py

---
+++ corel modules/ASSAIFI.py
@@ -0,0 +1,33 @@
+class ASSAIFI(nn.Module):
+    """Defines a single layer of the transformer encoder."""
+
+    def __init__(self, c1, cm=2048, num_heads=8, dropout=0.0, act=nn.GELU(), normalize_before=False):
+        """Initialize the TransformerEncoderLayer with specified parameters."""
+        super().__init__()
+        self.assa = AdaptiveSparseSA(c1, num_heads=num_heads, sparseAtt=True)
+        # Implementation of Feedforward model
+        self.fc1 = nn.Conv2d(c1, cm, 1)
+        self.fc2 = nn.Conv2d(cm, c1, 1)
+
+        self.norm1 = LayerNorm(c1)
+        self.norm2 = LayerNorm(c1)
+        self.dropout = nn.Dropout(dropout)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.act = act
+        self.normalize_before = normalize_before
+
+    def forward_post(self, src, src_mask=None, src_key_padding_mask=None, pos=None):
+        """Performs forward pass with post-normalization."""
+        BS, C, H, W = src.size()
+        src2 = self.assa(src).permute(0, 2, 1).view([-1, C, H, W]).contiguous()
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+        src2 = self.fc2(self.dropout(self.act(self.fc1(src))))
+        src = src + self.dropout2(src2)
+        return self.norm2(src)
+
+    def forward(self, src, src_mask=None, src_key_padding_mask=None, pos=None):
+        """Forward propagates the input through the encoder module."""
+        return self.forward_post(src, src_mask, src_key_padding_mask, pos)
